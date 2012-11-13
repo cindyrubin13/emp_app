40a1b8d69ce6664300a6aae48a670fbce2c8533c
Cindy Rubin <mjmrubin@aol.com>
2012-11-12 19:51:16 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmpApp::Application.config.secret_token = '3116180173b4df4f0b4b7532f0ec90b9ee4cf11f0e201e984d23877c2cfe3f94541188b21420dd126d7cd25059c18e6388e1988b4d176beba898e48999995273'
